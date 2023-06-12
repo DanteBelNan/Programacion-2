3708bebf9196be02f78ab831d542a9bc42f62ba4
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-06-12 17:58:44 -0300

    tp empresa cosecha terminado y aviones pasajeros empezado

---
+++ 8- Modelos de Segundo Parcial/Modelo Aviones, pasajeros, pasajes y vuelos/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+#include <string>
+#include "parcial2.h"
+
+
+int Apunto1(Pasajero reg){
+    FILE *p = fopen("punto1.dat", "ab");
+
+      if (p == NULL)
+      {
+        return -1;
+      }
+
+      int resultado = fwrite(&reg, sizeof(Pasajero), 1, p);
+      fclose(p);
+      return resultado;
+}
+
+void punto1(){
+    ArchivoPasajeros AP;
+    ArchivoPasajes APjes;
+
+    int tamAP = AP.contarRegistros();
+    int tamAPjes = APjes.contarRegistros();
+
+    for(int i = 0;i<tamAP;i++){
+        Pasajero pasajero;
+        pasajero = AP.leerRegistro(i);
+        bool compro2023 = false;
+        for(int i2 = 0;i2<tamAPjes;i2++){
+            Pasaje pasaje;
+            pasaje = APjes.leerRegistro(i2);
+
+            if(pasajero.getNumeroPasajero() == pasaje.getNumeroPasajero()){
+                if(pasaje.getFechaCompra().getAnio() == 2023){
+                    compro2023 = true;
+                }
+            }
+        }
+
+        if(!compro2023){
+            Apunto1(pasajero);
+        }
+
+
+    }
+}
+
+int modificarAvion(Avion reg, int pos){
+  FILE *p = fopen("aviones.dat", "rb+");
+
+  if (p == NULL)
+  {
+    return -1;
+  }
+
+  fseek(p, pos * sizeof(Avion), SEEK_SET);
+  int resultado = fwrite(&reg, sizeof(Avion), 1, p);
+  fclose(p);
+  return resultado;
+}
+
+void punto2(){
+    ArchivoAvion AV;
+    int tamAV = AV.contarRegistros();
+    char marca[30] = "Boeing";
+
+    for(int i = 0;i<tamAV;i++){
+        Avion avion;
+        avion = AV.leerRegistro(i);
+        if(!strcmp(marca,avion.getMarca())){
+            int seats = avion.getCantidadAsientos();
+            seats *= 0.95;
+            avion.setCantidadAsientos(seats);
+            modificarAvion(avion,i);
+        }
+    }
+
+}
+int main()
+{
+    punto2();
+    return 0;
+}
