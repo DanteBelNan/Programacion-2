39e981bc3bb24d99ea5ce6618f8a0178a2087c33
Dante Beltran Nanziot <dantebelnan@gmail.com>
2023-06-29 17:29:00 -0300

    cereales y cosechas, punto A creadas clase custom y archivo

--- 8- Modelos de Segundo Parcial/Modelo de Cosecha y Cereales/main.cpp
+++ 8- Modelos de Segundo Parcial/Modelo de Cosecha y Cereales/main.cpp
@@ -2,8 +2,49 @@
 
 using namespace std;
 
+class CpuntoA{
+    private:
+        int codigoCereal;
+        char nombre[30];
+        int cantToneladas;
+
+    public:
+        CpuntoA(){
+            cantToneladas = 0;
+        }
 
 
+
+};
+
+class ApuntoA{
+    public:
+
+        int guardarRegistro(CpuntoA reg){
+        FILE *p = fopen("puntoa.dat", "ab");
+        
+        if (p == NULL)
+        {
+            return -1;
+        }
+    
+    int resultado = fwrite(&reg, sizeof(CpuntoA), 1, p);
+    fclose(p);
+    return resultado;
+    }
+
+    CpuntoA leerRegistro(int pos){
+        CpuntoA reg;
+        FILE *p;
+        p=fopen("archivo.dat", "rb");
+        if(p==NULL) return reg;
+        fseek(p, sizeof(CpuntoA)*pos,0);
+        fread(&reg, sizeof reg,1, p);
+        fclose(p);
+        return reg;
+    }
+
+};
 int main()
 {
     
